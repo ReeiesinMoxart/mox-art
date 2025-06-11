aa2b6d4792622f5977a643ece00fa79142c87691
ReeiesinMoxart <Reeies@proton.me>
2025-06-11 22:31:10 +0900

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,8 @@ export default function Home() {
   return (
     <div className="flex items-center justify-center h-screen bg-black">
       <h1
-        className="text-white text-4xl cursor-pointer"
-        onClick={() => window.location.href = '/home'}
+        className="text-white text-6xl font-bold cursor-pointer"
+        onClick={() => (window.location.href = '/home')}
       >
         MOX-ART
       </h1>
