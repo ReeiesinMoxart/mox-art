699492ecc952062a3ffc1ba8d496c31bca9dde73
ReeiesinMoxart <Reeies@proton.me>
2025-06-11 22:38:42 +0900

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
+  "./pages/**/*.{js,ts,jsx,tsx}",
+  "./styles/**/*.{js,ts,jsx,tsx}",
+  "./components/**/*.{js,ts,jsx,tsx}",
+  "./app/**/*.{js,ts,jsx,tsx}", // for Next.js 13+ if using /app
+]
+
   ],
   theme: {
     extend: {},
